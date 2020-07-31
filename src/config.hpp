7cdcc3e52eec75fa949978a344d017853cc79412
Joel Schumacher <joelschum@gmail.com>
2020-07-31 23:38:25 +0200

    Add missing config file

---
+++ src/config.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <optional>
+#include <string_view>
+
+using namespace std::literals;
+
+struct Config {
+    size_t tabWidth = 4;
+
+    bool renderWhitespace = true;
+    std::optional<std::string_view> spaceChar = "·"sv;
+    std::optional<std::string_view> newlineChar = "↵"sv;
+    std::optional<std::string_view> tabStartChar = "-"sv;
+    std::optional<std::string_view> tabMidChar = "-"sv;
+    std::optional<std::string_view> tabEndChar = ">"sv;
+
+    bool showLineNumbers = true;
+};
+
+extern Config config;
